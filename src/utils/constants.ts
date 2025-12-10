a3aeaadf0e72abe370c324aefb88cefd24ec3bef
Claire Kwong <clairebkwong@gmail.com>
2025-12-10 14:06:04 -0500

    feat: demo day

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const ONE_PLAYER_X_POSITIONS = [0.6, 1.1, 0.1, -0.2]; //[0.6, 0.1, 1.1, 1.1];
-export const TWO_PLAYER_X_POSITIONS = [0.6, 0.6, 1.3, -0.2]; //[0.6, 0.6, -0.2, 1.3]
+export const TWO_PLAYER_X_POSITIONS = [0.6, 0.6, 1.0, 0.2]; //[0.6, 0.6, -0.2, 1.3]
 export const BODY_SCALE = 0.5;
 export const DEBUG_MODE = true;
